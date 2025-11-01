47d6f9f025421f424385720b2ae41187c4cc998c
himanshi singh <hsingh45968@gmail.com>
2025-11-01 21:32:26 +0530

    user defined function

---
+++ lect11/tempCodeRunnerFile.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int magic(int n)
+{
+    int sum=0;
+    int b=n;
+    l1:
+    for(sum=0;b!=0;b=b/10)
+    {
+        int last=b%10;
+        sum=sum+last;
+    }
+    if(sum>=10)
+    {
+        b=sum;
+        
+        goto l1;
+    }
+    else
+    {
+        if(sum==1)
+            printf("magic");
+        else
+            printf("not magic");
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    magic(n);
+}
